eb461f81fa31d335366535398b38a661d36cbbf9
AzureAD\MatthewDeRolf <matt@aerialintel.com>
2018-09-20 17:33:48 -0500

    production

--- charliebrigittehair/src/App.js
+++ charliebrigittehair/src/App.js
@@ -4,17 +4,22 @@ import './App.css';
 
 class App extends Component {
     render() {
-        return (<div className="App">
-            <header className="App-header">
-                <img src={logo}
-                    className="App-logo"
-                    alt="logo" />
-                <h1 className="App-title" > CHARLIE BRIGITTE HAIR </h1> </header> 
-            <p className="App-intro" >
-                       CHARLIE BRIGITTE HAIR COMING SOON!</p> 
-            </div>
-                                        );
-                                    }
-                                }
-                                
+        return ( < div className = "App" >
+            <
+            header className = "App-header" >
+            <
+            img src = { logo }
+            className = "App-logo"
+            alt = "logo" / >
+            <
+            h1 className = "App-title" > CHARLIE BRIGITTE HAIR < /h1> </header >
+            <
+            p className = "App-intro" >
+            <
+            /p>  <
+            /div>
+        );
+    }
+}
+
 export default App;
