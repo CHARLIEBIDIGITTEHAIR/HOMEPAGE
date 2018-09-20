68ea3265c0593f9ed6c0da80e71affd66f7d6655
AzureAD\MatthewDeRolf <matt@aerialintel.com>
2018-09-20 17:10:59 -0500

    MD_PRODUCTION_COMMIT

--- charliebrigittehair/src/App.js
+++ charliebrigittehair/src/App.js
@@ -9,9 +9,9 @@ class App extends Component {
                 <img src={logo}
                     className="App-logo"
                     alt="logo" />
-                <h1 className="App-title" > Welcome to React </h1> </header> 
+                <h1 className="App-title" > CHARLIE BRIGITTE HAIR </h1> </header> 
             <p className="App-intro" >
-                        To get started, edit < code > src / App.js </code> and save to reload. </p> 
+                       CHARLIE BRIGITTE HAIR COMING SOON!</p> 
             </div>
                                         );
                                     }
