c455288c9da8def2a3bbe37656a4fb21ec4008a1
AzureAD\MatthewDeRolf <matt@aerialintel.com>
2018-09-20 18:00:50 -0500

    MDCOMMIT

--- charliebrigittehair/src/App.js
+++ charliebrigittehair/src/App.js
@@ -4,20 +4,15 @@ import './App.css';
 
 class App extends Component {
     render() {
-        return ( < div className = "App" >
-            <
-            header className = "App-header" >
-            <
-            img src = { logo }
-            className = "App-logo"
-            alt = "logo" / >
-            <
-            h1 className = "App-title" > CHARLIE BRIGITTE HAIR < /h1> </header >
-            <
-            p className = "App-intro" >
-            <
-            /p>  <
-            /div>
+        return ( <div className = "App" >
+            <header className = "App-header"> 
+                <img src = { logo } className = "App-logo" alt = "logo" />
+                {/* <h1 className = "App-title"> CHARLIE BRIGITTE HAIR
+               </h1> */} 
+            </header>
+            // <p className = "App-intro" >
+            </p>  
+            </div>
         );
     }
 }
